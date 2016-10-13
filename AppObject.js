990c9c00983b90e4a4452ae99daa7d14f15dc425
sammyarenson <9sarenson@stpatrick.org>
2016-10-13 13:04:57 -0500

    Hopefully I did well

--- AppObject.js
+++ AppObject.js
@@ -8,25 +8,32 @@ function App(n,m,s){
     //Declare the instance function open here.
     this.open = function(){
       this.state="actve";
+
     };
     //Declare the instance function sleep here.
     this.sleep = function(){
-      if this.state="active"
-      this.state="sleep"
+      if (this.state="active"){
+      this.state="sleep";
+      this.memory=this.memory*2;
+      }
     }
     //Declare the instance function active here.
     this.active = function(){
-
-    }
+      if (this.state="sleep"){
+      this.state="active";
+      this.memory=this.memory*2;
+      }
     //Declare the instance function close here.
     this.close = function(){
-
-    }
-
+      if (this.state/="null"){
+      this.state="null"
+      this.memory=0
+      }
+    };
 }
 
 //Use the constructor to create 4 App objects below.
-
+var Snapchat=new App(),99,"active");
 
 
 //Declare an array named appList, and place all 4 of your apps in that array.
